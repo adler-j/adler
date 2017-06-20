bcf9697ab1584f1845e3a08e19e1553f42bbd37f
Jonas Adler <jonasadl@kth.se>
2017-06-20 16:34:12 +0200

    ENH: Cleanup to activations

--- adler/tensorflow/activation.py
+++ adler/tensorflow/activation.py
@@ -3,25 +3,18 @@ with demandimport.enabled():
     import tensorflow as tf
 
 
-def leaky_relu(_x, alpha=0.2):
-    with tf.name_scope('leaky_relu'):
-        alphas = tf.Variable(tf.constant(alpha,
-                                         shape=[int(_x.get_shape()[-1])]),
-                             trainable=False)
+def leaky_relu(_x, alpha=0.2, name='leaky_relu'):
+    return prelu(_x, init=alpha, name=name, trainable=False)
 
+
+def prelu(_x, init=0.0, name='prelu', trainable=True):
+    with tf.variable_scope(name):
+        alphas = tf.get_variable('alphas',
+                                 shape=[int(_x.get_shape()[-1])],
+                                 initializer=tf.constant_initializer(init),
+                                 dtype=tf.float32,
+                                 trainable=True)
         pos = tf.nn.relu(_x)
-        neg = alphas * (_x - tf.abs(_x)) * 0.5
-
-        return pos + neg
-
-
-def prelu(_x, init=0.0):
-    with tf.name_scope('prelu'):
-        alphas = tf.Variable(tf.constant(init,
-                                         shape=[int(_x.get_shape()[-1])]),
-                             trainable=True)
-
-        pos = tf.nn.relu(_x)
-        neg = alphas * (_x - tf.abs(_x)) * 0.5
+        neg = -alphas * tf.nn.relu(-_x)
 
         return pos + neg
